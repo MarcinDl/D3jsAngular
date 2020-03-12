39baa667eb918de246694eccace0890926ca1e00
Marcin Dlubis <marcindl@hotmail.com>
2020-03-12 11:08:01 +0100

    angular D3js

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { ChartComponent } from "./components/chart/chart.component";
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'd3-ng5-demo';
+}
